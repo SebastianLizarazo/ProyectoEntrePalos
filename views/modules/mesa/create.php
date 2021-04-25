3a59439d003561d2734215082bfdf4d08e816a69
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-04-25 14:35:07 -0500

    Modificacion del formulario de la vista create del modulo mesa

--- views/modules/mesa/create.php
+++ views/modules/mesa/create.php
@@ -65,41 +65,47 @@ $frmSession = $_SESSION['frm'.$pluralModel] ?? NULL; //Nombre del formulario (fr
                                 </div>
                             </div>
                             <!-- /.card-header -->
-                            <!-- form start -->
                             <div class="card-body">
+                            <!-- form start -->
                                 <form class="form-horizontal" method="post" id="frmCreate<?= $nameModel ?>"
-                                      name="frmCreate<?= $nameModel ?>"
-                                      action="../../../app/Controllers/MainController.php?controller=<?= $pluralModel ?>&action=create">
-                                    <div class="form-group row">
-                                        <label for="Numero" class="col-sm-2 col-form-label">Numero de mesa</label>
-                                        <div class="col-sm-10">
-                                            <input required type="number" class="form-control" id="Numero" name="Numero"
-                                                   placeholder="Ingrese el numero de mesa" value="<?= $frmSession['Numero'] ?? '' ?>">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label for="Ubicacion" class="col-sm-2 col-form-label">Ubicacion Mesa</label>
-                                        <div class="col-sm-10">
-                                            <input required type="text" class="form-control" id="Ubicacion" name="Ubicacion"
-                                                   placeholder="Ingrese la ubicacion de la mesa" value="<?= $frmSession['Ubicacion'] ?? '' ?>">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label for="Capacidad" class="col-sm-2 col-form-label">Capacidad de mesa</label>
-                                        <div class="col-sm-10">
-                                            <input required type="number" class="form-control" id="Capacidad" name="Capacidad"
-                                                   placeholder="Ingrese el la capacidad de la mesa" value="<?= $frmSession['Capacidad'] ?? '' ?>">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label for="Ocupacion" class="col-sm-2 col-form-label">Ocupacion</label>
-                                        <div class="col-sm-10">
-                                            <select required id="Ocupacion" name="Ocupacion" class="custom-select">
-                                                <option <?= ( !empty($frmSession['Ocupacion']) && $frmSession['Ocupacion'] == "Disponible") ? "selected" : ""; ?> value="Disponible">Disponible</option>
-                                                <option <?= ( !empty($frmSession['Ocupacion']) && $frmSession['Ocupacion'] == "Ocupada") ? "selected" : ""; ?> value="Ocupada">Ocupada</option>
-                                            </select>
-                                        </div>
-                                    </div>
+                                      name="frmCreate<?= $nameModel ?>" action="#">
+                                    <!--action="../../../app/Controllers/MainController.php?controller=--><?= ""//= $pluralModel ?><!--&action=create">-->
+
+
+                                      <div class="row">
+                                          <div class="col-sm-12">
+                                                <div class="form-group row">
+                                                    <label for="Numero" class="col-sm-2 col-form-label">Numero de mesa</label>
+                                                    <div class="col-sm-10">
+                                                        <input required type="number" class="form-control" id="Numero" name="Numero"
+                                                               placeholder="Ingrese el numero de mesa" value="<?= $frmSession['Numero'] ?? '' ?>">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="Ubicacion" class="col-sm-2 col-form-label">Ubicacion Mesa</label>
+                                                    <div class="col-sm-10">
+                                                        <input required type="text" class="form-control" id="Ubicacion" name="Ubicacion"
+                                                               placeholder="Ingrese la ubicacion de la mesa" value="<?= $frmSession['Ubicacion'] ?? '' ?>">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="Capacidad" class="col-sm-2 col-form-label">Capacidad de mesa</label>
+                                                    <div class="col-sm-10">
+                                                        <input required type="number" class="form-control" id="Capacidad" name="Capacidad"
+                                                               placeholder="Ingrese el la capacidad de la mesa" value="<?= $frmSession['Capacidad'] ?? '' ?>">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <label for="Ocupacion" class="col-sm-2 col-form-label">Ocupacion</label>
+                                                    <div class="col-sm-10">
+                                                        <select required id="Ocupacion" name="Ocupacion" class="custom-select">
+                                                            <option <?= ( !empty($frmSession['Ocupacion']) && $frmSession['Ocupacion'] == "Disponible") ? "selected" : ""; ?> value="Disponible">Disponible</option>
+                                                            <option <?= ( !empty($frmSession['Ocupacion']) && $frmSession['Ocupacion'] == "Ocupada") ? "selected" : ""; ?> value="Ocupada">Ocupada</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                          </div>
+                                      </div>
                                     <hr>
                                     <button type="submit" class="btn btn-info">Enviar</button>
                                     <a href="index.php" role="button" class="btn btn-default float-right">Cancelar</a>
