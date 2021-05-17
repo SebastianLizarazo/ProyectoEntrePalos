801f2e5ae7fb91df3ff438c4458ed395512b7058
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-05-17 14:26:33 -0500

    Commit de prueba

---
+++ test/testImagen.php
@@ -0,0 +1,43 @@
+<?php
+
+require ('../app/Models/Imagenes.php');
+use App\Models\Imagenes;
+
+$arrImage1 = [
+
+    'Nombre' => 'Imagen Hamburguesa 1',
+    'Descripcion' => 'Foto simple del producto hamburguesa doble',
+    'Ruta' => 'c:proyecto\imagenes\imagen1.png',
+    'Estado' => 'Activo',
+    'Producto_id' => ,
+    'Oferta_id' => ,
+
+];$arrImage1 = [
+
+    'Nombre' => 'Imagen Hamburguesa 2',
+    'Descripcion' => 'Foto simple del producto hamburguesa triple',
+    'Ruta' => 'c:proyecto\imagenes\imagen2.png',
+    'Estado' => 'Activo',
+    'Producto_id' => ,
+    'Oferta_id' => ,
+
+];$arrImage1 = [
+
+    'Nombre' => 'Imagen Alitas BBQ 1',
+    'Descripcion' => 'Foto simple del producto alitas BBQ medianas',
+    'Ruta' => 'c:proyecto\imagenes\imagen3.png',
+    'Estado' => 'Activo',
+    'Producto_id' => ,
+    'Oferta_id' => ,
+
+];$arrImage1 = [
+
+    'Nombre' => 'Imagen Gaseosa Coca Cola',
+    'Descripcion' => 'Foto de la gaseosa Coca Cola en baso de vidrio',
+    'Ruta' => 'c:proyecto\imagenes\imagen1.png',
+    'Estado' => 'Activo',
+    'Producto_id' => ,
+    'Oferta_id' => ,
+
+];
+
