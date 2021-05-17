e6f75ee80474289818120ebd1456ee811d81ef6f
K20022021 <kjmolano1@misena.edu.co>
2021-05-17 12:59:35 -0500

    Lista clase Marca con test probado, falta probar Pago

--- test/testMarca.php
+++ test/testMarca.php
@@ -3,44 +3,48 @@ require("..\app\Models\Marcas.php");
 
 use App\Models\Marcas;
 
-$arrMarca = [
-
+$arrMarca1 = [
     'Nombre' => 'panzote',
     'Descripcion' => 'Pan',
-    'Proveedor id' => '3',
+    'Proveedor_id' => '3',
     'Estado' => 'Inactiva',
 ];
 $arrMarca2 = [
-    'Nombre' => 'panzote',
-    'Descripcion' => 'Pan',
-    'Proveedor id' => '3',
+    'Nombre' => 'Campollo',
+    'Descripcion' => 'Pollo',
+    'Proveedor_id' => '1',
     'Estado' => 'Inactiva',
 ];
+$arrMarca3 = [
+    'Nombre' => 'Campollo',
+    'Descripcion' => 'Alitas',
+    'Proveedor_id' => '1',
+    'Estado' => 'Activa',
+];
+//$objMarca1 = new Marcas($arrMarca1);
+//var_dump($objMarca1);
+//$objMarca1->insert();
 
-$objMarca = new Marcas($arrMarca);
-var_dump($objMarca);
-$objMarca->insert();
+//$objMarca1->setNombre("Zafran"); //Cambio Valores
+//$objMarca1->setDescripcion("Guacamole"); //Cambio Valores
+//$objMarca1->update();
 
-//$objMarca->setNombre("Zafran"); //Cambio Valores
-//$objMarca->setDescripcion("Guacamole"); //Cambio Valores
-//$objMarca->update();
-
-//$objMarca->deleted();
+//$objMarca1->deleted();
 
 //$objMarca2 = new Marcas($arrMarca2);
 //$objMarca2->insert();
 
-//$arrResult = Marcas::search("SELECT * FROM marca WHERE Nombre = 'Bimbo'");
+//$arrResult = Marcas::search("SELECT * FROM marca WHERE Nombre = 'Panzote'");
 //if (!empty($arrResult)) {
-  // /* @var $arrResult Marcas[] */
+  ///* @var $arrResult Marcas[] */
     //foreach ($arrResult as $marca) {
       //  echo "Nombre: " . $marca->getId() . " - " . $marca->getNombre() . "\n";
     //}
 //}
 //$arrMarca2 = Marcas::searchForId(3);
 //if (!empty($arrMarca2)) {
-  //$arrMarca2->setEstado('Inactivo');
-    //$arrMarca2->update();
+  //$arrMarca2->setEstado('Inactiva');
+  //  $arrMarca2->update();
 //}
 //$arrMarc = Marcas::getAll();
 //$arrMarc = Marcas::getAll();
@@ -50,5 +54,13 @@ $objMarca->insert();
       //  echo "id: " . $Marcas->getId() . ", Nombre: " . $Marcas->getNombre() . ", Descripcion de la marca: " . $Marcas->getDescripcion() . ", Estado: " . $Marcas->getEstado() . "\n";
  //}
 //}
+
 //$arrMarca2 = Marcas::searchForId(5);
 //echo json_encode($arrMarca2);
+
+//$objectMarca3= new Marcas( $arrMarca3);
+//var_dump($objectMarca3);
+//$objectMarca3->insert();
+
+//$pruebaMarcaRegist= Marcas::marcaRegistrada('panzote',1);// Comprobamos que ya exista una marca con esas caracteristicas
+//var_dump($pruebaMarcaRegist);
