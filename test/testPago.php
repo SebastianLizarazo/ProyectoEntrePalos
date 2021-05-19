1e2b0ed9fa4e30e0dd39e1570adc92933fbd8daf
Bladimir Eslava Barrera <beslava@misena.edu.co>
2021-05-19 09:50:12 -0500

    Modificacion clase oferta

--- test/testPago.php
+++ test/testPago.php
@@ -16,38 +16,42 @@ $arrPago2 = [
     'Fecha' => '2021-01-05',
     'Estado' => 'Saldado',
 ];
+
 $objPago = new Pagos($arrPago); // Creamos un usuario... Pero no echo nada con el.
+//var_dump($objPago);
 $objPago->insert(); //Registramos el objeto en la base de datos
 
-$objPago->setTrabajadorId(3); //Cambio Valores
-$objPago->setFecha("2021-01-10"); //Cambio Valores
-$objPago->update();
+//$objPago->setTrabajadorId(3); //Cambio Valores
+//$objPago->setFecha("2021-01-10"); //Cambio Valores
+//$objPago->update();
 
-$objPago->deleted();
+//$objPago->deleted();
 
-$arrPago2 = new Pagos($arrPago2);
-$arrPago2->insert();
+$objPago2 = new Pagos($arrPago2);
+$objPago2-> insert();
 
-$arrResult = Pagos::search("SELECT * FROM pago WHERE Trabajador_id = 3 ");
-if (!empty($arrResult)) {
-    /* @var $arrResult Pagos[] */
-    foreach ($arrResult as $pagos) {
-        echo "Numero de Trabajador: " . $pagos->getId() . " - " . $pagos->getTrabajadorId() . "\n";
-    }
-}
-$arrPago2 = Pagos::searchForId(3);
-if (!empty($arrPago2)) {
-    $arrPago2->setEstado('Saldado');
-    $arrPago2->update();
-}
+//$arrResult = Pagos::search("SELECT * FROM pago WHERE Trabajador_id = 3 ");
+//if (!empty($arrResult)) {
+//    /* @var $arrResult Pagos[] */
+//    foreach ($arrResult as $pagos) {
+//        echo "Numero de Trabajador: " . $pagos->getId() . " - " . $pagos->getTrabajadorId() . "\n";
+//    }
+//}
 
-$arrPgs = Pagos::getAll();
-$arrPgs = Pagos::getAll();
-if (!empty($arrPgs)) {
-    /* @var $arrPgs Pagos[] */
-    foreach ($arrPgs as $pagos) {
-        echo "id: " . $pagos->getId() . "Numero del Trabajador: " . $pagos->getTrabajadorId() . ", Fecha: " . $pagos->getFecha() . ", Estado: " . $pagos->getEstado() . "\n";
-    }
-}
-$arrPago2 = Pagos::searchForId(5);
-echo json_encode($arrPago2);
+//$arrPago2 = Pagos::searchForId(3);
+//if (!empty($arrPago2)) {
+//    $arrPago2->setEstado('Saldado');
+//    $arrPago2->update();
+//}
+
+//$arrPgs = Pagos::getAll();
+//$arrPgs = Pagos::getAll();
+//if (!empty($arrPgs)) {
+//    /* @var $arrPgs Pagos[] */
+//    foreach ($arrPgs as $pagos) {
+//        echo "id: " . $pagos->getId() . "Numero del Trabajador: " . $pagos->getTrabajadorId() . ", Fecha: " . $pagos->getFecha() . ", Estado: " . $pagos->getEstado() . "\n";
+//    }
+//}
+
+//$arrPago2 = Pagos::searchForId(5);
+//echo json_encode($arrPago2);
