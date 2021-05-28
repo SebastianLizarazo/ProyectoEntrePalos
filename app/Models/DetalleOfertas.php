355df259dc2ddb3be0b9b3e3f9f9cbbe762e0ea2
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2021-05-28 09:47:28 -0500

    Error detalle oferta solucionado

--- app/Models/DetalleOfertas.php
+++ app/Models/DetalleOfertas.php
@@ -33,7 +33,7 @@ class DetalleOfertas extends AbstractDBConnection implements Model
 
     public static function detalleOfertaRegistrada(mixed $Producto_id, mixed $Oferta_id): bool
     {
-        $msaTmp = DetalleOfertas::search("SELECT * FROM detalleoferta WHERE Producto id = '$Producto_id' and Oferta id = '$Oferta_id'");
+        $msaTmp = DetalleOfertas::search("SELECT * FROM detalleoferta WHERE Producto_id = '$Producto_id' and Oferta_id = '$Oferta_id'");
         return (!empty($msaTmp)) ? true : false;
     }
 
@@ -160,7 +160,7 @@ class DetalleOfertas extends AbstractDBConnection implements Model
     {
         $query = "UPDATE detalleoferta SET 
             Producto_id = :Producto_id, Oferta_id = :Oferta_id, 
-            CantidadProducto = :CantidadProducto,WHERE id = :id";
+            CantidadProducto = :CantidadProducto WHERE id = :id";
         return $this->save($query);
     }
 
